0b597aec48d9f143c52ed792e12a42377cd4cbb2
Samuel-Brr <samuel.barre972@gmail.com>
2025-01-25 13:24:31 +0100

    init

---
+++ chat-support/src/main/java/com/yourcaryourway/chat_support/ChatSupportApplication.java
@@ -0,0 +1,13 @@
+package com.yourcaryourway.chat_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSupportApplication.class, args);
+	}
+
+}
---
+++ chat-support/src/test/java/com/yourcaryourway/chat_support/ChatSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcaryourway.chat_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
